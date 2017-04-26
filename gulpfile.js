34b540a12daecda90889735e7494cc63c8f56232
Sushil L <sushil_samson@hotmail.com>
2017-04-26 12:41:18 +1000

    gulp imports

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp')
+
+var less require('gulp-less')
+var gp_concat = require('gulp-concat')
+var gp_rename = require('gulp-rename')
+var gp_uglify = require('gulp-uglify')
+var minifyCSS = require('gulp-minify-css')
+var autoprefixer = require('gulp-autoprefixer')
