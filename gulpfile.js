44c6a0b16b5d9168708eb82887be0d97fabf02b2
Sushil L <sushil_samson@hotmail.com>
2017-04-26 13:30:04 +1000

    build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,43 @@
 var gulp = require('gulp')
-
-var less require('gulp-less')
+var less = require('gulp-less')
 var gp_concat = require('gulp-concat')
 var gp_rename = require('gulp-rename')
 var gp_uglify = require('gulp-uglify')
 var minifyCSS = require('gulp-minify-css')
 var autoprefixer = require('gulp-autoprefixer')
+var path = require('path')
+
+gulp.task('css', function(){
+  return gulp.src(
+    [
+      './public/assets/css/main.css',
+      './public/assets/css/ie8.css',
+      './public/assets/css/font-awesome.min.css'
+    ]
+  )
+  .pipe(minifyCSS())
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+  .pipe(gp_concat('style.min.css'))
+  .pipe(gulp.dest('./public/build/css/'))
+})
+
+gulp.task('build', function(){
+  return gulp.src(
+    [
+      './public/assets/js/jquery.min.js',
+      './public/assets/js/jquery.poptrox.min.js',
+      './public/assets/js/skel.min.js',
+      './public/assets/js/util.js',
+      './public/assets/js/ie/respond.min.js',
+      './public/assets/js/main.js',
+    ]
+  )
+  .pipe(gp_concat('gulp-concat.js'))
+  .pipe(gulp.dest('./public/min/'))
+  .pipe(gp_rename('vendor.min.js'))
+  .pipe(gp_uglify())
+  .pipe(gulp.dest('./public/build/'))
+})
+
+
+gulp.task('default', ['css', 'build'], function(){})
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,19 @@ module.exports = {
     sourceMapFilename: 'public/build/bundle.map'
   },
   devtool:'#source-map',
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize:true,
+      compress:{
+        warnings: true
+      }
+    })
+  ] : [],
   module: {
     loaders: [
       {
